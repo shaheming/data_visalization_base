5a7a08e649784562fd88b5a5276b358077bf32de
hemingsha <shm@djcard.cn>
2018-03-17 21:04:49 +0800

    Fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,14 +3,11 @@ class WelcomeController < ApplicationController
 
   def index
     @text = Time.now.to_s + "SHAME"
-    logger.info {"=====#{Time.now.to_s}======="}
   end
 
   def test
     render :json => {msg: "hello word! test  ww"}
   end
-
-
 
   private
 
