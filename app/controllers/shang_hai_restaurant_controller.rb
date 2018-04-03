11543d32e360e89cd422f14dd6e4b3592a72561d
hemingsha <shm@djcard.cn>
2018-04-03 09:28:01 +0800

    Fix

--- app/controllers/shang_hai_restaurant_controller.rb
+++ app/controllers/shang_hai_restaurant_controller.rb
@@ -1,22 +1,27 @@
 class ShangHaiRestaurantController < ApplicationController
   layout 'echart'
+  STYLE = "mapbox://styles/sandersaaa/cjficjvvld53d2qrq6o5q986n"
+  COLOR = ["#4A3F6A", "#55487A", "#5B4C82", "#825E89", "#91678C", "#9E6F8F", "#B98196", "#D1929C", "#F4AFA6", "#F5C6A0", "#F5D29A", "#F4DC93", "#F4E48C", "#F4E986"]
 
   def average
-
+    @map_style_path = STYLE
+    @color_range = COLOR
   end
 
   def total
-
+    @map_style_path = STYLE
+    # @color_range = ["#F4E986", "#F4E48C", "#F4DC93", "#F5D29A", "#F5C6A0", "#F4AFA6", "#D1929C", "#B98196", "#9E6F8F", "#91678C", "#825E89", "#5B4C82", "#55487A", "#4A3F6A"]
+    @color_range = COLOR
   end
 
   def average_data
     data = RestaurantDatum.avg_to_json
-    render :json => [data, data.size]
+    render :json => data
   end
 
 
   def total_data
     data = RestaurantDatum.total_to_json
-    render :json => [data, data.size]
+    render :json => data
   end
 end
--- app/models/restaurant_datum.rb
+++ app/models/restaurant_datum.rb
@@ -1,14 +1,14 @@
 class RestaurantDatum < ApplicationRecord
 
   def self.avg_to_json
-    self.all.map {
+    [self.all.map {
         |r| {name: r.name, value: [r.longitude.to_f, r.latitude.to_f, r.price.to_f]}
-    }
+    }, self.all.maximum(:price)]
   end
 
   def self.total_to_json
-    self.all.map {
+    [self.all.map {
         |r| {name: r.name, value: [r.longitude.to_f, r.latitude.to_f, r.price.to_f * r.review_cnt]}
-    }
+    }, self.all.maximum("price * review_cnt")]
   end
 end
